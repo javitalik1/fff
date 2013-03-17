cd790dc4dde410a1437ff7cd56b23cbaf8d3381e
javitalik1 <vitalik@chereda.net>
2013-03-17 21:45:00 +0200

    Update index.php

--- tmp/index.php
+++ tmp/index.php
@@ -1,16 +1,3 @@
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/xampp/');
-	exit;
-?>
-<<<<<<< HEAD
-branch master
-=======
-Something is wrong with the XAMPP installation :-(|) :-(|) :-(|)
-new_branch
->>>>>>> new_branch
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) 
+
